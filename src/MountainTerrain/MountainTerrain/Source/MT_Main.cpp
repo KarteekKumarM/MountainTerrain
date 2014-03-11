456a0303673e2e4722703ca51cd7840c13dbe6af
Karteek Mekala <Karteek.Kumar.M@gmail.com>
2014-03-10 22:41:02 -0700

    Introducing Logger

--- src/MountainTerrain/MountainTerrain/Source/MT_Main.cpp
+++ src/MountainTerrain/MountainTerrain/Source/MT_Main.cpp
@@ -1,5 +1,5 @@
 #include <Windows.h>
-#include <MT_Window.h>
+#include "MT_Window.h"
 
 // Entry point for Windows program
 int WINAPI WinMain(HINSTANCE hInstance, 
@@ -8,17 +8,17 @@ int WINAPI WinMain(HINSTANCE hInstance,
 				   int nCmdShow
 				   ) 
 {
-					   // create window
-					   MT_Window *window = new MT_Window();
-					   window->Init(hInstance, nCmdShow);
+	// create window
+	MT_Window *window = new MT_Window();
+	window->Init(hInstance, nCmdShow);
 
-					   // enter message loop
-					   int ret = window->EnterMessageLoop();
+	// enter message loop
+	int ret = window->EnterMessageLoop();
 
-					   // clean up
-					   window->Clean();
-					   delete window;
-					   window = 0;
+	// clean up
+	window->Clean();
+	delete window;
+	window = 0;
 
-					   return ret;
+	return ret;
 }
